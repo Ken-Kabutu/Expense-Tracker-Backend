ce662a56b531580964da3eb2ca6c776e0d086c23
maimuna omar <maymunaabdi07@gmail.com>
2023-09-05 18:57:27 +0300

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# db/seeds.rb
+
+# Create some expense categories
+expense_categories = ExpenseCategory.create([
+  { name: 'Food' },
+  { name: 'Transportation' },
+  { name: 'Entertainment' }
+])
+
+puts 'Expense categories created.'
+
+# Create a sample user with password_digest
+user = User.create(
+  name: 'John Doe',
+  email: 'john@example.com',
+  password_digest: BCrypt::Password.create('password') # Hash the password
+)
+
+puts 'User created.'
+
+# Create some sample expenses associated with the user and expense categories
+expenses = Expense.create([
+  {
+    user: user,
+    expense_category: expense_categories[0],
+    amount: 25.50,
+    date: Date.today - 7,
+    description: 'Dinner at a restaurant'
+  },
+  {
+    user: user,
+    expense_category: expense_categories[1],
+    amount: 10.00,
+    date: Date.today - 5,
+    description: 'Bus fare'
+  },
+  {
+    user: user,
+    expense_category: expense_categories[2],
+    amount: 15.00,
+    date: Date.today - 3,
+    description: 'Movie ticket'
+  }
+])
+
+puts 'Expenses created.'
+
+# You can add more seed data as needed.
+
+puts 'Seed data populated successfully.'
