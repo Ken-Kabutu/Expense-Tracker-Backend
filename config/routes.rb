bab83053b486624b488c0fe4dd9bc7d2b2b35ae8
maimuna omar <maymunaabdi07@gmail.com>
2023-09-05 18:54:09 +0300

    updated routes

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :expenses, only: [:index, :show, :create, :update, :destroy]
+  
+  
+  post '/users/register', to: 'users#register'
+  post '/users/login', to: 'users#login'
+  end
+  
