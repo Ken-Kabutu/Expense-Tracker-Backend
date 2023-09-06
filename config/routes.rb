047efcaad27808ace107e45ef50ba31dbc5815f9
Ken <kabutukenneth3@gmail.com>
2023-09-07 00:29:05 +0300

    add changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :expenses, only: [:index, :show, :create, :update, :destroy]
   
   
-  post '/users/register', to: 'users#register'
+  post '/users/regisert', to: 'users#register'
   post '/users/login', to: 'users#login'
+
   end
   
