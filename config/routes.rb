b0a48a591107987468325dd0c832081441dd8167
maimuna omar <maymunaabdi07@gmail.com>
2023-09-07 01:39:12 +0300

    updated users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   resources :expenses, only: [:index, :show, :create, :update, :destroy]
   
-  
   post '/users/register', to: 'users#register'
   post '/users/login', to: 'users#login'
-  get '/users', to: 'users#index'        # to get all users
-  get '/users/:id', to: 'users#show'     # to get a user by ID
-  end
-  
+  delete '/users/logout', to: 'users#logout'
+  get '/users/logged_in', to: 'users#logged_in_status'
+  resources :users, only: [:index, :show]
+end
