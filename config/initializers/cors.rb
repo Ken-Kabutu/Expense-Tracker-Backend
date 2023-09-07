dcb51c7d5eafa7b6aa4ea8a189e9028f10b73c86
alfred kimata <alfyjason@gmail.com>
2023-09-07 22:10:37 +0300

    backend code updated

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,13 +17,12 @@
 # config/initializers/cors.rb
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'http://localhost:5173' # Replace with the actual origin of your frontend
-  
-      resource '/api/*',
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true
-    end
+  allow do
+    origins "http://localhost:5173"
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
-  
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,58 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 # db/seeds.rb
 
-# Create some expense categories
-expense_categories = ExpenseCategory.create([
-  { name: 'Food' },
-  { name: 'Transportation' },
-  { name: 'Entertainment' }
-])
+# Seed data for users table
+User.create(name: 'John Doe', email: 'johndoe@example.com', password_digest: BCrypt::Password.create('password123'))
+User.create(name: 'Jane Smith', email: 'janesmith@example.com', password_digest: BCrypt::Password.create('password456'))
+# Add more users as needed
 
-puts 'Expense categories created.'
-
-# Create a sample user with password_digest
-user = User.create(
-  name: 'John Doe',
-  email: 'john@example.com',
-  password_digest: BCrypt::Password.create('password') # Hash the password
-)
-
-puts 'User created.'
-
-# Create some sample expenses associated with the user and expense categories
-expenses = Expense.create([
-  {
-    user: user,
-    expense_category: expense_categories[0],
-    amount: 25.50,
-    date: Date.today - 7,
-    description: 'Dinner at a restaurant'
-  },
-  {
-    user: user,
-    expense_category: expense_categories[1],
-    amount: 10.00,
-    date: Date.today - 5,
-    description: 'Bus fare'
-  },
-  {
-    user: user,
-    expense_category: expense_categories[2],
-    amount: 15.00,
-    date: Date.today - 3,
-    description: 'Movie ticket'
-  }
-])
-
-puts 'Expenses created.'
-
-# You can add more seed data as needed.
-
-puts 'Seed data populated successfully.'
+# You can also use Faker gem to generate random data for testing purposes
+# Uncomment the following lines if you have the Faker gem installed
+# 10.times do
+#   User.create(
+#     name: Faker::Name.name,
+#     email: Faker::Internet.email,
+#     password_digest: BCrypt::Password.create(Faker::Internet.password)
+#   )
+# end
